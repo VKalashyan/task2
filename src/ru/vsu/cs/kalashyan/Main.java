4747f54a3a7ae5bb7b223f23efebf9bd5f0d9003
Vladimir Kalashyan <vova.kalashyan5@mail.ru>
2020-10-07 21:08:32 +0300

    fix bugs

--- src/ru/vsu/cs/kalashyan/Main.java
+++ src/ru/vsu/cs/kalashyan/Main.java
@@ -15,7 +15,8 @@ public class Main {
             EquationRoot result = findRootsQuadraticEquation(a, b, c);
             System.out.println(" Answer x1 = " + result.x1);
             System.out.println(" Answer x2 = " + result.x2);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             System.out.println(" No roots ");
         }
     }
@@ -31,7 +32,8 @@ public class Main {
             double x1 = (-b - Math.sqrt(d)) / (2 * a);
             double x2 = (-b + Math.sqrt(d)) / (2 * a);
             return new EquationRoot(x1, x2);
-        } else {
+        }
+        else {
             throw new Exception(" Discriminant less than 0 ");
         }
     }
